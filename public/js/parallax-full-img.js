306306d2e2cdd427824a054ffc2b997b296bdc07
Pauline <pauline.frare-pradal@laposte.net>
2021-05-04 16:33:12 +0200

    style: update parallax full img effect (bug -> overflow:hidden)

--- public/js/parallax-full-img.js
+++ public/js/parallax-full-img.js
@@ -1,5 +1,8 @@
+// var image = document.getElementsByClassName('parallax-full');
+// new simpleParallax(image, {
+//     delay: .6,
+//     transition: 'cubic-bezier(0,0,0,1)'
+// });
+//
 var image = document.getElementsByClassName('parallax-full');
-new simpleParallax(image, {
-    delay: .6,
-    transition: 'cubic-bezier(0,0,0,1)'
-});
+new simpleParallax(image);
