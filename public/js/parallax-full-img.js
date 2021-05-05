e3222749f4d330031b7f523240a9ede4334bd6ab
Pauline <pauline.frare-pradal@laposte.net>
2021-05-05 11:27:02 +0200

    fix: bug pagination style and add text animation

--- public/js/parallax-full-img.js
+++ public/js/parallax-full-img.js
@@ -1,8 +1,2 @@
-// var image = document.getElementsByClassName('parallax-full');
-// new simpleParallax(image, {
-//     delay: .6,
-//     transition: 'cubic-bezier(0,0,0,1)'
-// });
-//
 var image = document.getElementsByClassName('parallax-full');
 new simpleParallax(image);
---
+++ public/js/txtanimation.js
@@ -0,0 +1,24 @@
+const output = document.querySelector('#output')
+const text = '   La civilisation des Incas a laissé une impression durable dans l’histoire du continent sud-américain.\n' +
+    '                    Pourtant, son âge d’or a duré à peine 100 ans. Pourquoi une telle place dans l’histoire alors ? Sans\n' +
+    '                    doute parce qu’ils étaient là quand les conquistadors espagnols sont arrivés, mais aussi parce\n' +
+    '                    qu’ils ont su faire la synthèse des cultures précédentes.'
+
+const ResetState = (event) => {
+    output.innerHTML = ''
+    DisplayText(text)
+}
+
+const DisplayText = line => {
+    let charPosition = 0
+    const waitForMilliseconds = 25
+
+    setInterval(() => {
+        if (charPosition < line.length) {
+            output.innerHTML += line.charAt(charPosition)
+            charPosition++
+        }
+    }, waitForMilliseconds)
+}
+
+DisplayText(text)
